fbf0a021b9a46122e2a1e785e9b94e9cef7cd5de
khuang <hy45360843@gmail.com>
2019-05-16 17:00:27 +0800

    add babel-polyfill

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Kent from './test';
+import 'babel-polyfill';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
